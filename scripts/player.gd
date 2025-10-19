90d3c14b2a967510947f24614f5af87977408b7f
Flap <shuruiguo1@gmail.com>
2025-10-20 08:18:53 +1100

    bug fxiing and fish works to boost speed

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,23 +3,23 @@ var initial_p = Vector2(1.0, 0.0)
 var notfloor = 0.0
 var gravity = 2000.0
 var jump = -800.0
-const speed = 200.0
 func _physics_process(delta):
 	velocity.x = 0
 	if Input.is_action_pressed("right"):
-		velocity.x = speed
+		velocity.x = e.speed
 	elif Input.is_action_pressed("left"):
-		velocity.x = -speed
+		velocity.x = -e.speed
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		notfloor += delta
-	if notfloor >= 10.0:
+	if is_on_floor():
 		notfloor = 0.0
+	if notfloor >= 5:
 		get_tree().change_scene_to_file("res://scenes/level.tscn")
 		position = initial_p
 		e.score = 1
-	if is_on_floor():
-		notfloor = 0.0
+		notfloor = 0
+		print (notfloor)
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = jump
 	move_and_slide()
